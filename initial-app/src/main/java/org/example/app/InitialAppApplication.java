85e2834ca4d0246d71430e1e8b5103b3d37b068b
Andreas Falk <info@andreas-falk.de>
2024-07-19 19:46:22 +0200

    Initial app

---
+++ initial-app/src/main/java/org/example/app/InitialAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialAppApplication.class, args);
+    }
+
+}
